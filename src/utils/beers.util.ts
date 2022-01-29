dd35e9059c4e6aa98dec5d5d635c2a1b00b9b0a1
Agusti Fernandez <agusti@colony.io>
2022-01-29 23:25:07 +0100

    chore: add some basic comments to the beer-o-meter function

--- src/utils/beers.util.ts
+++ src/utils/beers.util.ts
@@ -1,13 +1,16 @@
 const BeerOMeter = (temperature: number, people: number): number => {
   let beers = 0
-  if (people <= 1) throw Error('🍺 Not a real meeting, you have to be with at least 2 people to have a beer 🍺')
+  // Frontend will never call the API with least than 2 people, but just in case.
+  if (people <= 1)
+    throw Error('🍺 Not a real meeting, you have to be with at least 2 people to have a beer 🍺')
+  // Beer calculations go here, if it grew it could be a switch statement instead of a bunch of if statements.
   if (temperature < 20)
     beers = people * 1
-  if (temperature >= 20 && temperature <= 24)
+  else if (temperature >= 20 && temperature <= 24)
     beers = people * 2
-  if (temperature > 24)
+  else if (temperature > 24)
     beers = people * 3
-
+  // returns number of beers
   return beers;
 }
 
